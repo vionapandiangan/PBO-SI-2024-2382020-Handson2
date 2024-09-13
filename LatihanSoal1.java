231ace8d14584461ae43fb222a1c6ca0cfcf2c0e
Viona Pandiangan <2382020@unai.edu>
2024-09-13 12:45:29 +0700

    Handson2

--- LatihanSoal1.java
+++ LatihanSoal1.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+
+public class LatihanSoal1 {
+}import java.util.Scanner; // Perbaikan: Scanner dengan huruf besar dan ditambah tanda ';'
+
 public class LatihanSoal1 {
     public static void main(String[] args) {
         // Membuat Scanner untuk mengambil input dari user
@@ -11,7 +15,7 @@ public class LatihanSoal1 {
         System.out.println("Masukkan angka kedua: ");
         double num2 = input.nextDouble();
 
-        //Menentukan nilai yang paling besar
+        // Menentukan nilai yang paling besar
         if (num1 > num2) {
             System.out.println("Nilai terbesar adalah: " + num1);
         } else if (num2 > num1) {
@@ -22,6 +26,6 @@ public class LatihanSoal1 {
 
         // Menutup Scanner
         input.close();
-
     }
 }
+
